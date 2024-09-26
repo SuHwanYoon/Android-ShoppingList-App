e00755d2acfd64dd8ecddcdf65d74e140e2c9e03
SuHwanYoon <ehtldy@naver.com>
2024-09-26 20:28:04 +0900

    add ShoppingList DialogUI

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
     <img src="app/src/main/res/images/AddItemButton.png" alt="AddItemButton" width="300"/>
     <img src="app/src/main/res/images/alertDialog.png" alt="alertDialog" width="300"/>
 </div>
+<div style="display: inline-block;">
+    <img src="app/src/main/res/images/DialogUI.png" alt="DialogUI" width="300"/>
+    <img src="app/src/main/res/images/inputValueDialog.png" alt="inputValueDialog" width="300"/>
+</div>
--- app/src/main/java/yoon/tutorials/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/yoon/tutorials/shoppinglistapp/ShoppingList.kt
@@ -2,13 +2,15 @@ package yoon.tutorials.shoppinglistapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,13 +21,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ShoppingListApp(){
+fun ShoppingListApp() {
     //JetpackCompose에서 ShoppingItem 타입의 list를 사용하는 객체상태를 관리하는 변수선언
+    // Kotlin listOf는 기본적으로 불변이다 리스트를 변경하고싶다면 MutableList로 선언해야한다
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     //Add Item버튼을 눌렀을때 Dialog창의 표시상태여부를 가지는 변수 선언
     var showDialog by remember { mutableStateOf(false) }
+    //itemName 변수 초기값 빈문자열
+    var itemName by remember { mutableStateOf("") }
+    //itemQuantity변수 숫자형태의 String이 텍스트박스에 들어가기 때문에 초기값 String으로 설정
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         //컬럼이 화면을 가득채우도록
@@ -48,20 +54,98 @@ fun ShoppingListApp(){
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
-        ){
+        ) {
             //LazyColumn 내부항목
             //위에서 선언한 sItems항목을 렌더링하는  LazyListScope.items함수
-            items(sItems){
+            items(sItems) {
 
             }
         }
     }
 
-    if (showDialog){
+    if (showDialog) {
         //버튼말고 다른곳을 눌렀을때 showDialog를 flase로
         //AlertDialog는 기본적으로 나타날때 배경이 어두워짐
-        AlertDialog(onDismissRequest = { showDialog = false }) {
-            Text(text = "This is AlertDialog!")
-        }
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            //대화상자 확인버튼
+            confirmButton = {
+                //수평 영역
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    //Row에 위치하는 수평요소가 서로 간격을 두도록
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    //추가버튼
+                    Button(onClick = {
+                        //Add 버튼을 클릭했을때 itemName이 비어있지않다면
+                        if (itemName.isNotBlank()){
+                            //새로운 아이템 생성하고
+                            val newItem = ShoppingItem(
+                                //id는 현재 아이템목록크기에서 +1
+                                id = sItems.size+1,
+                                //name은 텍스트필드에 입력한 텍스트
+                                name = itemName,
+                                //텍스트필드에 입력한 String type 숫자를 int로 변환
+                                quantity = itemQuantity.toInt()
+                                //idEditing프로퍼티는 수정할때 ture가 되기때문에 생성시에는 false 기본값 으로 생성
+                            )
+                            //Add 버튼을 클릭했을때 itemName이 비어있지않다면
+                            //쇼핑목록 상태에 새로 생성한 아이템을추가
+                            sItems = sItems + newItem
+                            //대화상자를 닫기
+                            showDialog = false
+                            //새로운 아이템이 추가후 다시 대화상자가 열렸을때 텍스트필드 비우기
+                            itemName = ""
+
+                        }
+                    }) {
+                        Text(text = "Add")
+                    }
+                    //취소버튼 -> 대화상자 닫기
+                    Button(onClick = { showDialog = false }) {
+                        Text(text = "Cancel")
+                    }
+                }
+            },
+            //alert창의 제목
+            title = { Text(text = "Add Shopping Item") },
+            //alert창의 내용
+            text = {
+                Column {
+                    //itemName텍스트 필드
+                    OutlinedTextField(
+                        value = itemName,
+                        //textfield에 입력되는값(it)이 변할때마다 value속성의 값으로 설정
+                        onValueChange = { itemName = it },
+                        //textfield에 한줄로만 입력가능
+                        singleLine = true,
+                        //OutlinedTextField의 좌우넓이를 최대로하고 padding설정ㄴㅇ
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        //placeHolder
+                        label = { Text(text = "Enter ItemName!") }
+                    )
+                    //itemQuantity텍스트 필드
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        //textfield에 입력되는값(it)이 변할때마다 value속성의 값으로 설정
+                        onValueChange = { itemQuantity = it },
+                        //textfield에 한줄로만 입력가능
+                        singleLine = true,
+                        //OutlinedTextField의 좌우넓이를 최대로하고 padding설정
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        //placeHolder
+                        label = { Text(text = "Enter ItemQuantity!") }
+                    )
+                }
+            }
+
+        )
     }
 }
